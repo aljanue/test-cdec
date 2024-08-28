4386093751a41d41d9a97ebc0ba4e0b7c6456631
Alberto Játiva <134947748+aljanue@users.noreply.github.com>
2024-08-28 16:35:25 +0200

    update: fonts and screenshot

--- app.js
+++ app.js
@@ -7,42 +7,91 @@ const front_options = [
     document.getElementById("option-4"),
 ];
 current_answer=0;
+result = 0;
 
 const questions = [
     {
         question: "¿Dónde preferirías hacer un freetour?",
         options: [
-            "/src/paris.jpeg",
-            "/src/paris.jpeg",
-            "/src/paris.jpeg",
-            "/src/paris.jpeg",
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.42
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 1.19
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.81
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 2.5
+            },
         ]
     },
     {
         question: "¿Cuál es tu merchan favorito?",
         options: [
-            "/src/.jpeg",
-            "/src/.jpeg",
-            "/src/.jpeg",
-            "/src/.jpeg",
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 1.82
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 1.06
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 1.76
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 2.5
+            },
         ]
     },
     {
         question: "¿Con qué exturista harías dupla creativa?",
         options: [
-            "/src/.jpeg",
-            "/src/.jpeg",
-            "/src/.jpeg",
-            "/src/.jpeg",
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.12
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.65
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.43
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 2.5
+            },
         ]
     },
     {
         question: "¿Cuál es la cocina de tus sueños?",
         options: [
-            "/src/.jpeg",
-            "/src/.jpeg",
-            "/src/.jpeg",
-            "/src/.jpeg",
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.2
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 1.1
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 0.3
+            },
+            {
+                url: "/src/images/paris.jpeg",
+                puntuation: 2.5
+            },
         ]
     },
     // Agrega más preguntas aquí
@@ -76,24 +125,36 @@ function showAnswer(){
         title_question.textContent = questions[current_answer].question;
     }
     for(let i = 0; i < front_options.length; i++){
-        front_options[i].src = questions[current_answer].options[i];
+        front_options[i].src = questions[current_answer].options[i].url;
     }
 }
 function showResult(){
     nextContainer("test-container", "result-container");
-
-
 }
 
 function nextAnswer(){
-    console.log("antes de sumar: "+current_answer);
     current_answer++;
-    console.log("despues de sumar: "+current_answer);
-    console.log(questions.length);
     if(current_answer < questions.length)
         showAnswer();
     else{
         showResult();
         moveSticker(2);
     }
+}
+
+function addResult(x){
+    result+=questions[current_answer].options[x].puntuation;
+    console.log(result);
+}
+
+
+
+function captureAndDownload() {
+    html2canvas(document.body).then(canvas => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'soy_tour.png';
+        
+        link.click();
+    });
 }
